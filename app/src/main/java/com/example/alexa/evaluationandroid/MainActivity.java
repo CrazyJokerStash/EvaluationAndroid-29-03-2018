72c08f958f27ba2d1f3a70724c320ead5a1da2ae
Alexandre Abruzzese <alexandre.abruzzese@ynov.com>
2018-03-29 11:16:33 +0200

    First Commit

---
+++ app/src/main/java/com/example/alexa/evaluationandroid/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.alexa.evaluationandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private Intent data;
+    private ListView List;
+    private Button ajout;
+    private ArrayList<String> tab = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        List = findViewById(R.id.list);
+        ajout = findViewById(R.id.add);
+        tab.add("Aller faire les courses");
+        tab.add("Aller chercher son fils");
+        data = getIntent();
+        ajout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, save.class);
+                startActivityForResult(intent, 123);
+            }
+        });
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, tab);
+        List.setAdapter(adapter);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 123) {
+            if (resultCode == save.RESULT_OK) {
+                String value = data.getExtras().getString("valeur");
+                if (value != null)
+                    tab.add(value);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/alexa/evaluationandroid/save.java
@@ -0,0 +1,51 @@
+package com.example.alexa.evaluationandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by alexa on 29/03/2018.
+ */
+
+public class save extends AppCompatActivity
+{
+    private Button Save;
+    private EditText edit;
+    private Spinner spin;
+    List<String> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.courses);
+        Save = findViewById(R.id.save);
+        edit = findViewById(R.id.edit);
+        spin = findViewById(R.id.spinner);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        list.add("Normal");
+        list.add("High");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(dataAdapter);
+        Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(save.this, MainActivity.class);
+                intent.putExtra("valeur", edit.getText().toString());
+                setResult(save.RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
